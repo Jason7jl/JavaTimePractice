f2ac61c54ecf36dae32d83d6eb7b904017f58e05
William <vas069@icloud.com>
2022-07-12 18:13:13 -0700

    I did execises with LocalDateTime

--- README.md
+++ README.md
@@ -9,7 +9,14 @@ To start practicing time in java.time package go to exercises and start practici
 # Theory 
 
 All these abbreviations refer to the same time:
-    UTC time - Coordinated Universal Time. Prior to 1972, this time was called Greenwich Mean Time (GMT) but is now referred to as Coordinated Universal Time or Universal Time Coordinated (UTC).
-    Zulu time - Zulu comes from the US military, which assigns global time zones with letter codes. The UTC time zone carries the letter Z, which in the phonetic alphabet is expressed as “Zulu.”
+    UTC time - Coordinated Universal Time. Prior to 1972, this time was called Greenwich Mean Time (GMT) 
+    but is now referred to as Coordinated Universal Time or Universal Time Coordinated (UTC).
+    Zulu time - Zulu comes from the US military, which assigns global time zones with letter codes. 
+    The UTC time zone carries the letter Z, which in the phonetic alphabet is expressed as “Zulu.”
     GMT - Greenwich Mean Time
 
+
+# Questions
+
+1. In what methods do you use ChronoUnit?
+2. 
--- exercises/LocalTimePractice.java
+++ exercises/LocalTimePractice.java
@@ -3,23 +3,33 @@ import java.time.LocalTime;
 // 2. Practice with LocalTime class
 public class LocalTimePractice {
 
+    public static void main(String[] args) {
+
+        LocalTimePractice localTimePractice = new LocalTimePractice();
+
+        System.out.println(localTimePractice.getCurrTime());
+        System.out.println(localTimePractice.addHours(3));
+    }
+
     // 1. in previous module we practiced local date class and already know how to get current date
     // now you should try to get current time
     public LocalTime getCurrTime(){
-        LocalTime localTime = null; // modify this line to create current time
+        LocalTime localTime = LocalTime.now(); // modify this line to create current time
 
-        return null;
+        return localTime;
     }
 
     // 2. Add hours in time
     // On many occasions, we would like to add hours, minutes or seconds to calculate time in the future.
-    // Java 8 has not only helped with Immutable and thread-safe classes but also provided better methods e.g. plusHours() instead of add(), there is no conflict.
-    // hint: BTW, remember that these methods return a reference to new LocalTime instance because LocalTime is immutable, so don't forget to store them back.
+    // Java 8 has not only helped with Immutable and thread-safe classes but also provided better methods
+    // e.g. plusHours() instead of add(), there is no conflict.
+    // hint: BTW, remember that these methods return a reference to new LocalTime instance because LocalTime is immutable,
+    // so don't forget to store them back.
     public LocalTime addHours(Integer hours){
         LocalTime time = LocalTime.now();
         // adding hours print and return it
 
-        return null; // return the actual result here
+        return time.plusHours(hours); // return the actual result here
     }
 
 
